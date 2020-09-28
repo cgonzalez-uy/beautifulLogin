47d85fe6da9c7aa612893dd6dcf5029e3221f8f9
cgonzalez-uy <cgonzalez.uy@gmail.com>
2020-09-28 18:07:05 -0300

    withour environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false, // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  firebase : {
-    apiKey: 'AIzaSyBE0NgUpxivibk0LFfEUpsnVI2NwW8cTkE',
-    authDomain: 'medium-turtlecode.firebaseapp.com',
-    databaseURL: 'https://medium-turtlecode.firebaseio.com',
-    projectId: 'medium-turtlecode',
-    storageBucket: 'medium-turtlecode.appspot.com',
-    messagingSenderId: '782536361415',
-    appId: '1:782536361415:web:71e0137b6ee8b249d285e2',
-    measurementId: 'G-LBXY2C4FSP'
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
